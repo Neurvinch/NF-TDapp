a59fc1fee08bf7d959bd62bd87d47addac6dee82
Naveen pandian <npandian515@gmail.com>
2025-08-14 15:32:56 +0530

    sserver .js is accurately seeted up

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const cors = require('cors')
+
+const connectDB = require('./config/db')
+
+
+const eventRoutes = require("./routes/eventRoutes")
+
+const app = express()
+app.use(express.json());
+
+app.use(cors());
+
+
+app.use("/api/events". eventRoutes)
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
