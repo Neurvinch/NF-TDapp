7e63be210571b01c59e7d227797ca09200ec5337
Naveen pandian <npandian515@gmail.com>
2025-08-14 15:14:09 +0530

    backend for our dapp models for tickets

--- backend/models/Ticket.js
+++ backend/models/Ticket.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose')
 
 const ticketSchema = new mongoose.Schema({
-    
-})
+
+    eventId: {type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true},
+    tokenId: {type: Number, required: true},
+    ownerWallet: {type: String, required: true},
+
+    checkedIn: {type: Boolean, default: false},
+}, {timestamps: true})
+
+
+module.exports = mongoose.model("Ticket", ticketSchema)
